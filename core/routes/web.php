b2ecd2feeafe3cbf7840a78317071bbf667cb5b3
Berserker <155231526+Berserker-k01@users.noreply.github.com>
2025-08-14 22:30:19 +0000

    Update web.php

--- core/routes/web.php
+++ core/routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', [SiteController::class, 'index'])->name('home');
 // Investment calculator endpoint used by home banner modal
 Route::get('/user/investment-calculate/{id}', [SiteController::class, 'investmentCalculate'])
     ->name('user.investmentcalculate');
+
+// Investment plan listing page (used by header menu)
+Route::get('/investment-plan', [SiteController::class, 'allInvestmentPlan'])
+    ->name('investmentplan');
